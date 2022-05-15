0738bf3590bfb0a022121379abab141f329c2438
Swann <princeapurbask@gmail.com>
2022-05-15 16:27:29 +0600

    CRUD operation for product

--- contorller/ProductController.js
+++ contorller/ProductController.js
@@ -15,3 +15,50 @@ export const getAllProducts = async (request, response) => {
         })
     }
 }
+
+export const getSingleProduct = async (request, response) => {
+    try {
+        const product = await Product.findById(request.query.id);
+        response.status(200).json({
+            status: true,
+            product: product
+        })
+    } catch (error) {
+        response.status(500).json({
+            status: false,
+            message: 'Failed to get single product'
+        })
+    }
+}
+
+export const updateSingleProduct = async (request, response) => {
+    try {
+        const product = await Product.findByIdAndUpdate(request.query.id, request.body);
+        response.status(200).json({
+            status: true,
+            product: product,
+            message: 'Product updated successfully'
+        })
+    } catch (error) {
+        response.status(500).json({
+            status: false,
+            message: 'Failed to update single product'
+        })
+    }
+}
+
+export const deleteSingleProduct = async (request, response) => {
+    try {
+        const product = await Product.findByIdAndDelete(request.query.id);
+        console.warn(product, 'Product deleted');
+        response.status(200).json({
+            status: true,
+            message: 'Product deleted successfully'
+        })
+    } catch (error) {
+        response.status(500).json({
+            status: false,
+            message: 'Failed to delete product'
+        })
+    }
+}
